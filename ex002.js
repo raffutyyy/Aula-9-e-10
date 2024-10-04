aa1ab326d95b3c838f70b1080a0731d33fd8fb5d
Raffaela <raffa.fdo@gmail.com>
2024-10-03 21:41:27 -0300

    Atividades da aula de quinta (03/10/24)

---
+++ ex002.js
@@ -0,0 +1,11 @@
+let frase = prompt('Escreva uma frase')
+
+let maiu = frase.toUpperCase()
+console.log(maiu)
+
+let novaFrase = maiu.replaceAll('O', 'I')
+console.log(novaFrase)
+
+//frase.
+console.log(frase.length)
+
---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Qual seu nome?")
+let cor = prompt("Qual sua cor favorita?")
+
+console.log(`A cor favorita de ${nome} é ${cor}`)
+
+//
+
+let nome2 = prompt("Qual seu nome?")
+let cor2 = prompt("qual sua cor favorita?")
+
+
+console.log(`A cor favorita de ` + nome2 + ' é ' + cor2)
